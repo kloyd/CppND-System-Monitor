6a48ec03c466f69dde187a4631153bd1e8586ffd
kloyd <kelly.loyd@icloud.com>
2020-04-17 16:53:59 -0500

    Format hh:mm:ss. First cut at MemInfo

--- src/format.cpp
+++ src/format.cpp
@@ -8,4 +8,25 @@ using std::string;
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
 // REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+string Format::ElapsedTime(long seconds) { 
+    long minutes = seconds / 60;
+    long elapsed_seconds = seconds % 60;
+    long hours = minutes / 60;
+    minutes = minutes % 60;
+
+    std::string str_hours = std::to_string(hours);
+    if (str_hours.length() < 2) {
+        str_hours = "0" + str_hours;
+    }
+    std::string str_minutes = std::to_string(minutes);
+    if (str_minutes.length() < 2) {
+        str_minutes = "0" + str_minutes;
+    }
+    std::string str_seconds = std::to_string(elapsed_seconds);
+    if (str_seconds.length() < 2) {
+        str_seconds = "0" + str_seconds;
+    }
+
+    return str_hours + ":" + str_minutes + ":" + str_seconds;
+    
+}
--- src/linux_parser.cpp
+++ src/linux_parser.cpp
@@ -2,6 +2,7 @@
 #include <unistd.h>
 #include <string>
 #include <vector>
+#include <iostream>
 
 #include "linux_parser.h"
 
@@ -67,7 +68,32 @@ vector<int> LinuxParser::Pids() {
 }
 
 // TODO: Read and return the system memory utilization
-float LinuxParser::MemoryUtilization() { return 0.0; }
+float LinuxParser::MemoryUtilization() { 
+  string line;
+  string key;
+  float memFree, memTotal;
+  string value;
+  std::ifstream filestream(kProcDirectory + kMeminfoFilename);
+  std::cerr << kProcDirectory + kMeminfoFilename;
+  if (filestream.is_open()) {
+    while (std::getline(filestream, line)) {
+      //std::replace(line.begin(), line.end(), ' ', '_');
+      //std::replace(line.begin(), line.end(), '=', ' ');
+      //std::replace(line.begin(), line.end(), '"', ' ');
+      std::istringstream linestream(line);
+      while (linestream >> key >> value) {
+        std::cerr << "key: " << key << ", value " << value << "\n";
+        if (key == "MemTotal:") {
+          memTotal = std::stof(value);
+        }
+        if (key == "MemFree:") {
+          memFree = std::stof(value);
+        }
+      }
+    }
+  }
+  return memTotal - memFree;
+}
 
 // TODO: Read and return the system uptime
 long LinuxParser::UpTime() { return 0; }
